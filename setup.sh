2faeb5ac2ed922e939380b7c13ba9841a089ab92
Akar Kaung <akarthut99@gmail.com>
2026-04-26 21:08:38 +0800

    update dependency

---
+++ setup.sh
@@ -0,0 +1,19 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y \
+    python3-pip \
+    python3-venv \
+    python3-dev \
+    ffmpeg \
+    libavformat-dev \
+    libavcodec-dev \
+    libavdevice-dev \
+    libavutil-dev \
+    libavfilter-dev \
+    libswscale-dev \
+    libswresample-dev \
+    libopus-dev \
+    libvpx-dev \
+    pkg-config \
+    libcamera-dev \
+    python3-picamera2
